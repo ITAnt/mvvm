6af1e2c9f438fa5553854c1c1fa06f9376954011
ITAnt <964161472@qq.com>
2024-09-14 09:56:44 +0800

    configs

---
+++ buildSrc/src/main/groovy/Config.groovy
@@ -0,0 +1,15 @@
+class Config {
+    static compileSdk = 29
+    static minSdk = 21
+    static targetSdk = 29
+    static buildTools = '30.0.3'
+    static test_junit = '4.12'
+    static test_ext = '1.1.2'
+    static test_espresso = '3.3.0'
+    // 用1.3.0解决ActivityForResult闪退的问题
+    static google_appcompat = '1.3.0'
+    static google_material = '1.1.0'
+    static ktx_core = '1.3.2'
+    static androidLifecycle = '2.2.0'
+    static rxLifecycle = '3.1.0'
+}
